6af7390e83e1f6dd48875cdd16d9cf7d61228a76
vendi <svitlana.vakulenko@gmail.com>
2018-02-28 14:18:58 +0100

    chg: set to set of entities shortest path sparql query constructor

--- hdt_topk.py
+++ hdt_topk.py
@@ -5,11 +5,19 @@ svakulenko
 
 Call shortest path algorithm SPARQL web service
 
-Sample SPARQL query: 
+Sample SPARQL queries: 
+
     PREFIX ppf: <java:at.ac.wu.arqext.path.>
     PREFIX dbr: <http://dbpedia.org/resource/>
     SELECT ?path 
     WHERE { ?path ppf:topk (dbr:IT_Security dbr:ABNT_NBR_15602 6)}
+
+    PREFIX ppf: <java:at.ac.wu.arqext.path.>
+    PREFIX dbr: <http://dbpedia.org/resource/>
+    SELECT ?path
+    WHERE { ?path ppf:topk (?X ?Y 2) . 
+    VALUES (?X ) { (dbr:Felipe_Massa)(dbr:1952_Winter_Olympics) } .
+    VALUES (?Y ) { (dbr:Elliot_Richardson) (dbr:Red_Bull) }  }
 '''
 
 import requests
@@ -19,18 +27,24 @@ TOPK_SERVICE = 'http://hdt.communidata.at/dbpedia/query'
 QUERY_TEMPLATE = '''
                 PREFIX ppf: <java:at.ac.wu.arqext.path.>
                 PREFIX dbr: <http://dbpedia.org/resource/>
-                SELECT ?path 
-                WHERE { ?path ppf:topk (dbr:%s dbr:%s %d)}
+                SELECT ?path
+                WHERE { ?path ppf:topk (?X ?Y %d) . 
+                VALUES (?X ) { %s } .
+                VALUES (?Y ) { %s }  }
                  '''
 
+ENTITY_TEMPLATE = "(dbr:%s)"
+
 SAMPLE_ENTITIES = [['IT_Security'], ['ABNT_NBR_15602']]
+
 
 def get_topk_paths(uttered_entities, response_entities, k=6):
     '''
     takes 2 lists of entities: uttered_entities and response_entities
     returns a list of paths
     '''
-    query = QUERY_TEMPLATE % (uttered_entities[0], response_entities[0], k)
+    query = QUERY_TEMPLATE % (k, ''.join([ENTITY_TEMPLATE % e for e in uttered_entities]),
+                                 ''.join([ENTITY_TEMPLATE % e for e in response_entities]))
     try:
         response = requests.get(TOPK_SERVICE, params={'query': query, 'output': 'json'})
         paths = response.json()['results']['bindings']
