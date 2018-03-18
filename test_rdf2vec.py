8246a5e437a0fc84d12a0ee44f72f5e1dc15b080
vendi <svitlana.vakulenko@gmail.com>
2018-03-18 12:32:32 +0100

    chg: test

--- test_rdf2vec.py
+++ test_rdf2vec.py
@@ -7,7 +7,7 @@ Created on Mar 18, 2016
 DBPEDIA_EMBS = './embeddings/data.dws.informatik.uni-mannheim.de/rdf2vec/models/DBpedia/2015-10/noTypes/db2vec_sg_200_5_25_5'
 
 
-def load_with_gensim(emb_path=DBPEDIA_EMBS):
+def load_iteratively(emb_path=DBPEDIA_EMBS):
     # from https://machinelearningmastery.com/use-word-embedding-layers-deep-learning-keras/
     embeddings_index = dict()
     f = open(emb_path)
@@ -29,3 +29,7 @@ def load_with_gensim(emb_path=DBPEDIA_EMBS):
     model = gensim.models.Word2Vec.load(emb_path)
     print ('dbr:Rocky')
     print (model.most_similar(positive=['dbr:Rocky'], topn=100))
+
+
+if __name__ == '__main__':
+    load_iteratively()
