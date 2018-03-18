d8baff9bbd8f8c1c7279086d41774417bd87e8d3
vendi <svitlana.vakulenko@gmail.com>
2018-03-18 12:30:32 +0100

    chg: load embeddings iteratively

---
+++ test_rdf2vec.py
@@ -0,0 +1,31 @@
+'''
+Created on Mar 18, 2016
+
+@author: svakulenko
+'''
+# 'DB2Vec_sg_500_5_5_15_2_500'
+DBPEDIA_EMBS = './embeddings/data.dws.informatik.uni-mannheim.de/rdf2vec/models/DBpedia/2015-10/noTypes/db2vec_sg_200_5_25_5'
+
+
+def load_with_gensim(emb_path=DBPEDIA_EMBS):
+    # from https://machinelearningmastery.com/use-word-embedding-layers-deep-learning-keras/
+    embeddings_index = dict()
+    f = open(emb_path)
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vector = asarray(values[1:], dtype='float32')
+        print word
+        print vector
+        return
+        embeddings_index[word] = vector
+    f.close()
+    print('Loaded %s word vectors.' % len(embeddings_index))
+
+
+def load_with_gensim(emb_path=DBPEDIA_EMBS):
+    # loads the whole embeddings model into memory
+    import gensim
+    model = gensim.models.Word2Vec.load(emb_path)
+    print ('dbr:Rocky')
+    print (model.most_similar(positive=['dbr:Rocky'], topn=100))
