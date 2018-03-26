26c335259dbb8bb9e0a334070dcf381e3d523cf3
vendi <svitlana.vakulenko@gmail.com>
2018-03-26 20:05:54 +0200

    fix: path

--- train_model_words.py
+++ train_model_words.py
@@ -30,7 +30,7 @@ negative_sampling_strategies = ['random', 'vertical', 'horizontal']
 for strategy in negative_sampling_strategies:
     # vertical negative sampling
     X_path_words = './%s/words_%s_X.npy' % (sample, strategy)
-    y_path = './%s/y_%s.npy' % (sample, strategy)
+    y_path = './%s/y.npy' % sample
 
     x_train, y_train, x_val, y_val, x_test, y_test, input_length = load_dataset_splits(X_path_words, y_path, test_split=0.2, validation_split=0.2)
 
