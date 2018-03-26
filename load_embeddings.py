f3b4a5c546c5788d2735933fc3d6c6a24bff7c46
Michael Cochez <cochez@bruegel>
2018-03-26 11:55:59 +0200

    Corrected the loading, made it more resilient and faster

--- load_embeddings.py
+++ load_embeddings.py
@@ -19,21 +19,24 @@ PATH = './embeddings_npy/'
 
 def load_embeddings(embeddings, embedding_matrix, vocabulary):
     words = 0
-    vocabulary_entities = vocabulary.keys()
+    #vocabulary_entities = vocabulary.keys()
     # embeddings in a text file one per line for Global vectors and glove word embeddings
     for line in embeddings:
-        values = line.split()
+        wordAndVector = line.split(None, 1)
         # match the entity labels in vector embeddings
-        word = values[0]
+        word = wordAndVector[0]
         word = word[1:-1]  # Dbpedia global vectors strip <> to match the entity labels 
-        print word
-        if word in vocabulary_entities:
-            embedding_vector = np.asarray(values[1:], dtype='float32')
+        #print word
+        if word in vocabulary:
+            vector = wordAndVector[1]
+            vector = vector.split()
+            embedding_vector = np.asarray(vector, dtype='float32')
             embedding_matrix[vocabulary[word]] = embedding_vector
-            
             words += 1
             if words >= len(vocabulary):
-                return embedding_matrix
+                break
+    missing = len(vocabulary)-words
+    print "done loading line based embedding. %d missing" % missing
     return embedding_matrix
 
 
@@ -46,6 +49,7 @@ def load_embeddings_lines(embeddings_config, label):
         embedding_matrix = load_embeddings(embs_file, embedding_matrix, vocabulary)
         # save embedding_matrix for entities in the training dataset
         np.save(PATH+label+'.npy', embedding_matrix)
+    #print embedding_matrix
     return embedding_matrix
 
 
@@ -55,29 +59,40 @@ def load_embeddings_gensim(embeddings_config, label):
     embedding_matrix = np.zeros((len(vocabulary)+1, embeddings_config['dims']))
         
     # load embeddings binary model with gensim for word2vec and rdf2vec embeddings
-    model = gensim.models.KeyedVectors.load_word2vec_format(embeddings_config['path'], binary=True)
+    model = gensim.models.Word2Vec.load(embeddings_config['path'])
+    #model = gensim.models.KeyedVectors.load_word2vec_format(embeddings_config['path'], binary=True)
     embedded_entities = model.wv
-    
+    missing = 0
     for entity, entity_id in vocabulary.items():
         # strip entity label format to rdf2vec label format
-        rdf2vec_entity_label = 'dbr:%s' % entity.split('/')[-1]
-        print rdf2vec_entity_label
+        #rdf2vec_entity_label = 'dbr:%s' % entity.split('/')[-1]
+        #print rdf2vec_entity_label
+        rdf2vec_entity_label = '<' + entity + '>'
         if rdf2vec_entity_label in embedded_entities:
-            embedding_matrix[entity_id] = model.wv[entity]
-
+            embedding_matrix[entity_id] = embedded_entities[rdf2vec_entity_label]
+        else:
+            missing += 1
+    print "done loading gensim entities. %d missing" % missing
     # save embedding_matrix for entities in the training dataset
     np.save(PATH+label+'.npy', embedding_matrix)
+    print embedding_matrix
     return embedding_matrix
 
 
 if __name__ == '__main__':
 
     for embeddings_name, config in embeddings['GlobalVectors'].items():
-        label = 'GlobalVectors_' + embeddings_name
-        print label
-        load_embeddings_lines(config, label)
+        try:
+            label = 'GlobalVectors_' + embeddings_name
+            print label
+            load_embeddings_lines(config, label)
+        except Exception as e:
+            print e
 
     for embeddings_name, config in embeddings['rdf2vec'].items():
-        label = 'rdf2vec_' + embeddings_name
-        print label
-        load_embeddings_gensim(config, label)
+        try:
+            label = 'rdf2vec_' + embeddings_name
+            print label
+            load_embeddings_gensim(config, label)
+        except Exception as e:
+            print e    
