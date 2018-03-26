77b63d87554305b0b661aa2e1f349543598597b4
vendi <svitlana.vakulenko@gmail.com>
2018-03-26 12:14:17 +0200

    fix

--- train_model_words.py
+++ train_model_words.py
@@ -22,9 +22,12 @@ batch_size = 128
 epochs = 5
 
 # dataset params
+sample = 'sample172098'
 vocabulary_size = 30541  # unique entities + extra token 0 for UNK
+X_path_words = './%s/words_X.npy' % sample
+y_path = './%s/y.npy' % sample
 
-x_train, y_train, x_val, y_val, x_test, y_test, input_length = load_dataset_splits(test_split=0.2, validation_split=0.2)
+x_train, y_train, x_val, y_val, x_test, y_test, input_length = load_dataset_splits(X_path_words, y_path, test_split=0.2, validation_split=0.2)
 
 for label, embeddings_config in word_embeddings.items():
     print label
