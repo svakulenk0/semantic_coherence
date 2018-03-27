3c2b914d5b205c540f9e8533ac9122f2d35e3816
vendi <svitlana.vakulenko@gmail.com>
2018-03-27 20:46:08 +0200

    chg: save architecture

--- train_model_words.py
+++ train_model_words.py
@@ -27,14 +27,13 @@ vocabulary_size = 30541  # unique entities + extra token 0 for UNK
 
 negative_sampling_strategies = ['random', 'vertical', 'horizontal']
 
-for strategy in negative_sampling_strategies:
-    # vertical negative sampling
+for strategy in negative_sampling_strategies[0:]:
     X_path_words = './%s/words_%s_X.npy' % (sample, strategy)
     y_path = './%s/y.npy' % sample
 
     x_train, y_train, x_val, y_val, x_test, y_test, input_length = load_dataset_splits(X_path_words, y_path, test_split=0.2, validation_split=0.2)
 
-    for embeddings_name, embeddings_config in word_embeddings.items():
+    for embeddings_name, embeddings_config in word_embeddings.items()[1:]:
         label = "%s_%s" % (strategy, embeddings_name)
         print label
         embeddings_config['matrix_path'] = PATH + embeddings_name + '.npy'
@@ -44,10 +43,10 @@ for strategy in negative_sampling_strategies:
         loss, accuracy = model.evaluate(x_test, y_test, verbose=1)
         print('Accuracy: %f' % (accuracy * 100))
 
-    # serialize the trained model to JSON
-    # model_json = model.to_json()
-    # with open("./models/%s_model_172098.json" % label, "w") as json_file:
-    #     json_file.write(model_json)
+        # serialize the trained model to JSON
+        model_json = model.to_json()
+        with open("./models/%s_model.json" % sample, "w") as json_file:
+            json_file.write(model_json)
     
     # # serialize weights to HDF5
     # model.save_weights('./models/%s_weights_172098.h5' % label)
