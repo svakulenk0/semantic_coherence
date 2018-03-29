ac0d7b7a6c7d6e29338db6c62d10ae1d9830a1dc
vendi <svitlana.vakulenko@gmail.com>
2018-03-29 15:37:32 +0200

    chg: train on new dataset

--- train_model_words.py
+++ train_model_words.py
@@ -22,20 +22,23 @@ batch_size = 128
 epochs = 20
 
 # dataset params
-sample = 'sample172098'
+sample = 'sample172098_new'
 vocabulary_size = 30541  # unique entities + extra token 0 for UNK
+# specify negative sampling strategies e.g. 'random', 'vertical', 'horizontal'
+negative_sampling_strategies = ['random']
+# specify embeddings, e.g. GloVe, word2vec
+embedding_names = ['GloVe']
 
-negative_sampling_strategies = ['random', 'vertical', 'horizontal']
-
-for strategy in negative_sampling_strategies[0:]:
+for strategy in negative_sampling_strategies:
     X_path_words = './%s/words_%s_X.npy' % (sample, strategy)
     y_path = './%s/y.npy' % sample
 
     x_train, y_train, x_val, y_val, x_test, y_test, input_length = load_dataset_splits(X_path_words, y_path, test_split=0.2, validation_split=0.2)
 
-    for embeddings_name, embeddings_config in word_embeddings.items()[1:]:
+    for embeddings_name in embedding_names:
         label = "%s_%s" % (strategy, embeddings_name)
         print label
+        embeddings_config = word_embeddings[embeddings_name]
         embeddings_config['matrix_path'] = PATH + embeddings_name + '.npy'
         model = train(x_train, y_train, x_val, y_val, vocabulary_size, input_length, embeddings_config, label, batch_size, epochs)
 
@@ -45,7 +48,7 @@ for strategy in negative_sampling_strategies[0:]:
 
         # serialize the trained model to JSON
         model_json = model.to_json()
-        with open("./models/%s_model.json" % sample, "w") as json_file:
+        with open("./models/%s_%s_model.json" % (sample, label), "w") as json_file:
             json_file.write(model_json)
     
     # # serialize weights to HDF5
