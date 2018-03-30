b1497223f35363f28d76316f0cf2a93ad245c138
vendi <svitlana.vakulenko@gmail.com>
2018-03-30 12:29:16 +0200

    fix: skip empty embeddings words

--- measure_word_distances.py
+++ measure_word_distances.py
@@ -5,7 +5,7 @@ svakulenko
 
 Estimate pair-wise distances between words in the conversation
 
-loaded embeddings for 18 words out of 19 
+loaded embeddings for 18 words out of 19 (except .tar.gz)
 ['OSX', 'compiz', 'zip', 'md5sum', 'Leopard', 'rc2', 'bluetooth', 'walkthrough', 'init', 'computer', 'file', 'ubuntu', 'battle', 'apt-get', 'spawning', 'runlevel', 'gdm', 'md5']
 '''
 import numpy as np
@@ -43,15 +43,16 @@ def measure_word_distances(sample=SAMPLE_WORDS_4606):
     
     for word in sample:
         print word
-        word_vector = embeddings[word]
-        print embedding_vector.shape
-        print previous_word_vectors.shape
-        # estimate distances from new word to all previous words
-        # compare with cosine between the new word vector and the word vectors of the previous words
-        if previous_word_vectors != [[]]:
-            word_distances = cosine_similarity(embedding_vector, previous_word_vectors)
-            words_distances.append(word_distances)
-        previous_word_vectors = np.append(previous_word_vectors, word_vector, axis=1)
+        if word in embeddings:
+            word_vector = embeddings[word]
+            print embedding_vector.shape
+            print previous_word_vectors.shape
+            # estimate distances from new word to all previous words
+            # compare with cosine between the new word vector and the word vectors of the previous words
+            if previous_word_vectors != [[]]:
+                word_distances = cosine_similarity(embedding_vector, previous_word_vectors)
+                words_distances.append(word_distances)
+            previous_word_vectors = np.append(previous_word_vectors, word_vector, axis=1)
     print words_distances
 
 
