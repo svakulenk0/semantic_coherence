d903a4ff1410552347960164873dd0e5593a9d33
vendi <svitlana.vakulenko@gmail.com>
2018-03-31 22:58:52 +0200

    new: load glove embeddings

--- load_embeddings.py
+++ load_embeddings.py
@@ -10,21 +10,35 @@ import numpy as np
 import gensim
 from keras.preprocessing.sequence import pad_sequences
 
-from process_ubuntu_dialogues import load_vocabulary, create_vocabulary
-from process_ubuntu_dialogues import load_annotated_dialogues
-from process_ubuntu_dialogues import load_dialogues_words
-from embeddings import entity_embeddings as embeddings
+from embeddings import entity_embeddings
+from embeddings import word_embeddings
+from prepare_dataset import load_vocabulary, LATEST_SAMPLE
+
 
 PATH = './embeddings_npy/'
 
-def load_embeddings(embeddings, embedding_matrix, vocabulary):
+
+def load_glove_word_embeddings(embeddings_name='GloVe', sample=LATEST_SAMPLE):
+    vocabulary = load_vocabulary('./%s/words/vocab.pkl' % sample)
+    # from https://machinelearningmastery.com/use-word-embedding-layers-deep-learning-keras/
+    # create a weight matrix for entities in training docs
+    embedding_matrix = np.zeros((len(vocabulary)+1, word_embeddings[embeddings_name]['dims']))
+    with open(word_embeddings[embeddings_name]['path']) as embs_file:
+        embedding_matrix = load_embeddings(embs_file, embedding_matrix, vocabulary, entities=False)
+        # save embedding_matrix for entities in the training dataset
+        np.save(PATH+'embeddings_name.npy', embedding_matrix)
+    print embedding_matrix
+
+
+def load_embeddings(embeddings, embedding_matrix, vocabulary, entities=True):
     words = 0
     # embeddings in a text file one per line for Global vectors and glove word embeddings
     for line in embeddings:
         wordAndVector = line.split(None, 1)
         # match the entity labels in vector embeddings
         word = wordAndVector[0]
-        word = word[1:-1]  # Dbpedia global vectors strip <> to match the entity labels 
+        if entities:
+            word = word[1:-1]  # Dbpedia global vectors strip <> to match the entity labels 
         #print word
         if word in vocabulary:
             vector = wordAndVector[1]
@@ -35,7 +49,7 @@ def load_embeddings(embeddings, embedding_matrix, vocabulary):
             words += 1
             if words >= len(vocabulary):
                 break
-    missing = len(vocabulary)-words
+    missing = len(vocabulary) - words
     print "done loading line based embedding. %d missing" % missing
     return embedding_matrix
 
@@ -77,8 +91,8 @@ def load_embeddings_gensim(embeddings_config, label, vocabulary):
     return embedding_matrix
 
 
-if __name__ == '__main__':
-    sample = 'sample172098'
+def load_entity_embeddings(sample=LATEST_SAMPLE):
+    # dataset params
     entity_vocabulary = load_vocabulary('./%s/vocab.pkl' % sample)
 
     for embeddings_name, config in embeddings['GlobalVectors'].items():
@@ -96,3 +110,7 @@ if __name__ == '__main__':
             load_embeddings_gensim(config, label, entity_vocabulary)
         except Exception as e:
             print e
+
+
+if __name__ == '__main__':
+    load_glove_word_embeddings()
