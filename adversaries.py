5ee2927a742287f09d56e80d8ba6fd9f6bf96159
vendi <svitlana.vakulenko@gmail.com>
2018-04-01 18:11:23 +0200

    new: vocabulary distributions

--- adversaries.py
+++ adversaries.py
@@ -11,9 +11,10 @@ from keras.preprocessing.sequence import pad_sequences
 
 from prepare_dataset import load_vocabulary
 from prepare_dataset import LATEST_SAMPLE
+from sample291848 import entity_distribution, word_distribution
 
 
-def generate_uniform_random(folder, sample=LATEST_SAMPLE, test='test/'):
+def generate_uniform_random(folder, sample=LATEST_SAMPLE, test='test/', **kwargs):
     '''
     Pick items from the vocabulry unifromly at random using the same length as of a positive example
     '''
@@ -31,7 +32,31 @@ def generate_uniform_random(folder, sample=LATEST_SAMPLE, test='test/'):
     np.save('./%s/%s/%srandom_X.npy' % (sample, folder, test), adversaries)
 
 
-def generate_sequence_disorder(folder, sample=LATEST_SAMPLE, test='test/'):
+def generate_vocabulary_distribution(folder, vocabulary_distribution, sample=LATEST_SAMPLE, test='test/'):
+    '''
+    Pick items from the vocabulry unifromly at random using the same length as of a positive example
+    '''
+    # load vocabulary
+    vocabulary = load_vocabulary('./%s/%s/vocab.pkl' % (sample, folder))
+    # load positive samples
+    positives = np.load('./%s/%s/%spositive_X.npy' % (sample, folder, test))
+
+    adversaries = []
+
+    # prepare probabilities from vocabulary counts distribution
+    entities = vocabulary_distribution.keys()
+    entities_counts = vocabulary_distribution.values()
+    entities_probs = [count / float(sum(entities_counts)) for count in entities_counts]
+
+    
+    for dialogue in positives:
+        adversaries.append(np.random.choice(entities, replace=False, size=len(dialogue), p=entities_probs))
+
+    assert len(adversaries) == len(positives)
+    np.save('./%s/%s/%srandom_X.npy' % (sample, folder, test), adversaries)
+
+
+def generate_sequence_disorder(folder, sample=LATEST_SAMPLE, test='test/', **kwargs):
     '''
     Randomly rearrange (permute) the original sequence (sentence ordering task)
     '''
@@ -49,11 +74,12 @@ def generate_sequence_disorder(folder, sample=LATEST_SAMPLE, test='test/'):
 
 
 def generate_adversaries(generator):
-    for folder in ['entities', 'words']:
+    vocabulary_distributions = [entity_distribution, word_distribution]
+    for i, folder in enumerate(['entities', 'words']):
         # development
-        generator(folder, test='')
+        generator(folder, test='', vocabulary_distribution=vocabulary_distributions[i])
         # test set
-        generator(folder)
+        generator(folder, vocabulary_distribution=vocabulary_distributions[i])
 
 
 if __name__ == '__main__':
