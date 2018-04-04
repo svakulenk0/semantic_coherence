f591c3e74a8f91c1626440d6ab97694282e698c7
vendi <svitlana.vakulenko@gmail.com>
2018-04-04 12:08:24 +0200

    new: parse folder

--- parse_paths.py
+++ parse_paths.py
@@ -53,7 +53,7 @@ import numpy as np
 from annotate_shortest_paths import PATH_SHORTEST_PATHS
 
 
-def parse_paths_folder(endpoint='widipedia', nfiles=10):
+def parse_paths_folder(endpoint='dbpedia', nfiles=10):
     '''
     Show most common relations and external entities
 
