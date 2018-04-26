0ffb77f98eafe6a70ba83a0087210fe5af38cf3e
vendi <svitlana.vakulenko@gmail.com>
2018-04-26 13:14:23 -0300

    fix: split training entity and word models

--- src/create_random_embeddings.py
+++ src/create_random_embeddings.py
@@ -17,11 +17,11 @@ def create_embeddings_random(dimension=200, voc='entities'):
     vocabulary <str> 'entities' or 'words'
     '''
     vocabulary = load_vocabulary('../data/%s/vocab.pkl' % voc)
-    label = "random_vectors_%s" % dimension
+    label = "random_vectors"
     try:
         print label
         # create a weight matrix for entities in training docs
-        embedding_matrix = np.zeros((len(vocabulary)+1, dimension))
+        embedding_matrix = np.zeros((len(vocabulary), dimension))
         for vocabulary_term in vocabulary.keys():
             embedding_vector = np.random.rand(dimension)
             embedding_matrix[vocabulary[vocabulary_term]] = embedding_vector
@@ -32,4 +32,7 @@ def create_embeddings_random(dimension=200, voc='entities'):
 
 
 if __name__ == '__main__':
+    # create random entity embeddings
     create_embeddings_random()
+    # create random word embeddings
+    create_embeddings_random(300, 'words')
